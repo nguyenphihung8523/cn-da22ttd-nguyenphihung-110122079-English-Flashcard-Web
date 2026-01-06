c342329c7e72f20f253f09d0f47e5ee46ce2ec54
nguyenphihung8523 <phihung336223@gmail.com>
2026-01-06 21:48:42 +0700

    Rename Flashcards tab to Bai hoc in admin

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -363,7 +363,7 @@ export default function AdminDashboard() {
                   : 'bg-gray-200 text-gray-800 hover:bg-gray-300'
               }`}
             >
-              📇 Flashcards
+              📚 Bài học
             </button>
             <button
               onClick={() => handleTabChange('speaking')}
